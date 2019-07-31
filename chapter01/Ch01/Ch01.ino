d1dd32882273d176c4871136428002faef3e8ede
jackjeanab <35165571+jackjeanab@users.noreply.github.com>
2019-07-31 16:52:40 +0800

    add file

---
+++ chapter01/Ch01/Ch01.ino
@@ -0,0 +1,24 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Wed, 31 Jul 2019 08:51:11 GMT
+ */
+
+
+
+void setup()
+{
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
